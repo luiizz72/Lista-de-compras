10fd8eadbe104ac43bae45b1109cad0f2a024c39
luiiz72 <luiz991675232@gmail.com>
2026-02-13 19:38:10 -0300

    Voltando ao inicio

--- variaveis.js
+++ variaveis.js
@@ -1,10 +1,39 @@
-const numeros = [5, 8, 6, 10, 7];
-let soma = 0;
+//TESTE DE VARIÁVEIS
+const funcionarios = [
+  ["Ana", "Juliana", "Fernando"],
+  [21, 17, 18]
+];
+console.log(`${funcionarios[0][2]} tem ${funcionarios[1][2]} anos`)
 
-for (let i = 0; i < numeros.length; i++) {
-    soma += numeros[i];
-}
+// TESTE DE CONCATENAÇÃO DE ARRAYS
+const menuPrincipal = ['arroz', 'feijoada', 'linguiça calabresa', 'farofa'];
+const salada = ['alface', 'tomate', 'cenoura', 'cebola'];
+const menuDeSobremesas = ['pudim', 'sorvete', 'bolo de morango'];
+const menuCompleto = menuPrincipal.concat(salada, menuDeSobremesas);
 
-const media = soma / numeros.length;
+// Lista de compras
+const listadeCompras = ['arroz', 'feijão', 'carne', 'frutas'];
+const itemRemovido = listadeCompras.pop('frutas'); // remove o último item
+const itemAdicionado = 'Coca cola';
+listadeCompras.push(itemAdicionado); // adiciona no final
 
-console.log("A média dos números é:", media);
+// Exibindo os resultados
+console.log('Lista de compras atualizada:', listadeCompras);
+console.log('Item removido:', itemRemovido);
+console.log('Menu completo:', menuCompleto);
+
+
+//teste de variáveis
+let i = 5;
+let valorA = i++;
+let valorB = ++i;
+
+console.log("O valor de valorA é:", valorA);
+console.log("O valor de valorB é:", valorB);
+
+
+//Essa função é pra depois, eu pesquisar pra entender melhor o que ela faz
+const notas = [7, 8, 9, 10, 6, 5, 4];
+for (let indice = 5; indice < notas.length; indice++){
+    console.log(indice, notas[indice]);
+}   
